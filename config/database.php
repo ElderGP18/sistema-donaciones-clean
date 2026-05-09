df64b5df6d8040db181e560b2a9a09c8232208f5
elder <ochurusbaltiano@gmail.com>
2026-05-09 11:18:21 -0600

    fix: mysqli_report OFF para compatibilidad PHP 8.1+ sin tablas

--- config/database.php
+++ config/database.php
@@ -8,9 +8,10 @@ define('DB_NAME', 'u949489569_Donacion2026');
 define('DB_CHARSET', 'utf8mb4');
 
 function getConnection(): mysqli {
+    mysqli_report(MYSQLI_REPORT_OFF);
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if ($conn->connect_error) {
-        die(json_encode(['error' => 'Error de conexión: ' . $conn->connect_error]));
+        die('Error de conexión: ' . $conn->connect_error);
     }
     $conn->set_charset(DB_CHARSET);
     return $conn;
